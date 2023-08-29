633ec2546d801440ba3af3ad6e3bc2adf1f439f5
ichbill <ichbill@163.com>
2023-08-29 14:32:05 -0500

    train

---
+++ model.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+class MouseModel(nn.Module):
+    def __init__(self, num_meta_features, num_classes):
+        super(MouseModel, self).__init__()
+
+        self.resnet = models.resnet18(pretrained=True)
+        self.resnet = nn.Sequential(*list(self.resnet.children())[:-1])
+        self.image_fc = nn.Linear(512, 256) 
+
+        self.meta_fc = nn.Sequential(
+            nn.Linear(num_meta_features, 128),
+            nn.ReLU(),
+            nn.Dropout(0.25),
+            nn.Linear(128, 256)
+        )
+
+        self.classifier = nn.Linear(256*2, num_classes)
+
+    def forward(self, image, meta):
+        batch_size, num_frames, c, h, w = image.shape
+        image = image.view(batch_size*num_frames, c, h, w)
+
+        image = self.resnet(image)
+        image = image.view(batch_size, num_frames, -1)
+        image = self.image_fc(image)
+        image = image.mean(dim=1) 
+
+        meta = meta.to(torch.float32)
+        meta = self.meta_fc(meta)
+        meta = meta.mean(dim=1)
+
+        combined = torch.cat((image, meta), dim=1)
+        
+        output = self.classifier(combined)
+        return output
