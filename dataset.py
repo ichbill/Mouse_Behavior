348df5e93efec0d4a63ed9b690b0af00c5a5d3a1
nikk <sharmanikhiljiit@gmail.com>
2023-09-19 16:42:13 -0500

    DDP with new audio features MFCC and LFCC

--- dataset.py
+++ dataset.py
@@ -5,19 +5,33 @@ from torchvision import transforms
 from torch.utils.data import Dataset, DataLoader
 from PIL import Image
 import torchaudio.functional as F
-
+import torch.nn.functional as Ff
+from scipy.signal import hilbert, chirp
+from pydub import AudioSegment
+from natsort import natsorted
+import torchaudio.transforms as T
 import json
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
+
+def audio_trimming(audio_path, trim_time):
+    audio = AudioSegment.from_file(audio_path)
+    start_time = trim_time * 1000 #As pydub deals in milliseconds
+    trimmed_audio = audio
+    sample_rate = trimmed_audio.frame_rate
+    audio_array = np.array(trimmed_audio.get_array_of_samples())
+    audio_tensor = torch.from_numpy(audio_array).float()
+
+    #print(audio_tensor.size())
+    return audio_tensor, sample_rate
 
 def one_hot_encode(labels):
-    one_hot_labels = [0,0,0]
-    if ' habituation' in labels:
-        one_hot_labels[0] = 1
+    one_hot_labels = [0, 0]
     if ' flinching' in labels:
-        one_hot_labels[1] = 1
+        one_hot_labels[0] = 1 
     if ' licking' in labels:
-        one_hot_labels[2] = 1
+        one_hot_labels[1] = 1
     return one_hot_labels
 
 def interploate_pose(pose_pred):
@@ -53,44 +67,187 @@ def interploate_pose(pose_pred):
 def time_to_frame(time):
     time_str = str(time)
     if len(time_str) > 5:
-        time_str = time_str[:5] # handle 29:48:00 in Formalin_acute_pain_1.csv
+        # print("First one", time_str)
+        time_str = time_str[1:6] # handle 29:48:00 in Formalin_acute_pain_1.csv
     if '.' in time_str:
+        # print("Second one", time_str)
         minutes, seconds = map(int, time_str.split('.'))
     elif ':' in time_str:
+        # print("Third one", time_str)
         minutes, seconds = map(int, time_str.split(':'))
     total_seconds = minutes * 60 + seconds
     return total_seconds * 30
 
-def sliding_window(pose_keypoints, audio, label_data, step=10, stride=10):
-    # sliding window for pose
+def sliding_window(pose_keypoints, audio, label_data, step=30, stride=10):
     bias = len(pose_keypoints)%step
-    print(pose_keypoints.shape)
-    print(bias)
-    print(pose_keypoints[bias:].shape)
-
     
-    print(audio.shape)
-    behavior_feat = np.array([pose_keypoints[bias:][i:i+step] for i in range(0,len(pose_keypoints[bias:]),stride)])
-    audio_feat = np.array([audio[bias:][i:i+step] for i in range(0,len(audio[bias:]),stride)])
+    subarrays = [] 
+    for i in range(bias, len(pose_keypoints), stride):
+        if len(pose_keypoints[i:i+step]) == 30:
+            subarray = pose_keypoints[i:i+step]  # Extract a subarray of length 'step'
+            subarrays.append(subarray)
+        else:
+            continue
 
-    # sliding window for labels
-    label_window = np.array([label_data[bias:][i:i+step] for i in range(0,len(label_data[bias:]),stride)])
+    behavior_feat = np.asarray(subarrays)
+    subarrays = []
+    for i in range(bias, len(audio), stride):
+        if len(audio[i:i+step]) == 30:
+            subarray = audio[i:i+step]  # Extract a subarray of length 'step'
+            subarrays.append(subarray)
+        else:
+            continue
+    
+    audio_feat = np.asarray(subarrays)
+    subarrays = []
+    for i in range(bias, len(label_data), stride):
+        if len(label_data[i:i+step]) == 30:
+            subarray = label_data[i:i+step]  # Extract a subarray of length 'step'
+            subarrays.append(subarray)
+        else:
+            continue
+
+    label_window = np.asarray(subarrays)
+    subarrays = []
+    
     one_hot_labels = np.array([one_hot_encode(list(set(x))) for x in label_window])
-    # one_hot_labels = np.array([one_hot_encode(x) for x in labels])
 
-    valid_indices = ~(np.all(one_hot_labels == [0,0,0], axis=1))
+    valid_indices = ~(np.all(one_hot_labels == [0,0], axis=1))
     behavior_feat = behavior_feat[valid_indices]
+    
     audio_feat = audio_feat[valid_indices]
     one_hot_labels = one_hot_labels[valid_indices]
-    # print(behavior_feat.shape, one_hot_labels.shape)
-    # breakpoint()
+
     return behavior_feat, audio_feat, one_hot_labels, valid_indices
+
+def listing(resampled_audio):
+    x = []
+    for i in range(0, len(resampled_audio), 1000):
+        arr = np.array(resampled_audio[i:i+1000])
+        if len(arr) == 1000:
+            x.append(arr)
+        else:
+            pass
+    x= np.asarray(x)
+    x = x[:-93, :]
+    x = torch.from_numpy(x)
+    return x
+
+def count_labels(labels):
+    count_linking = 0
+    count_flinching = 0
+    for i in range(len(labels)):
+        if labels[i,1] == 0:
+            count_linking = count_linking + 1
+        else:
+            count_flinching = count_flinching + 1
+    print(count_linking)
+    print(count_flinching)
+
+
+def mfcc_lfcc(sampled_array, sample_rate, n_fft=127, hop_length=16, n_lfcc = 64, win_length = None):
+    spectrogram = T.Spectrogram(n_fft=n_fft, hop_length=hop_length)
+    lfcc_spectrogram = T.LFCC(
+        sample_rate=sample_rate,
+        n_lfcc=n_lfcc,
+        speckwargs={
+            "n_fft": n_fft,
+            "win_length": win_length,
+            "hop_length": hop_length,
+        },
+    )
+    
+    audio_feat = []
+    # for i in tqdm(range(len(sampled_array)), desc="Processing"):
+    for i in tqdm(range(len(sampled_array))):
+        mfcc_spec = spectrogram(sampled_array[i])
+        lfcc_spec = lfcc_spectrogram(sampled_array[i])
+        
+        padding = [0, 1]
+        mfcc_spec = Ff.pad(mfcc_spec, padding, mode='constant', value=0)
+        mfcc_spec = mfcc_spec.numpy()
+        lfcc_spec = Ff.pad(lfcc_spec, padding, mode='constant', value=0)
+        lfcc_spec = lfcc_spec.numpy()
+        audio_feat.append([mfcc_spec, lfcc_spec])
+    return audio_feat
+
+def feature_extraction(audio_path):
+    waveform, sample_rate = audio_trimming(audio_path, 653)
+    resampled_audio = F.resample(waveform, sample_rate, 30000, rolloff=0.99)
+    sampled_array = listing(resampled_audio)
+    features = mfcc_lfcc(sampled_array, 30000)
+    features = np.asarray(features)
+    # features = torch.from_numpy(features)
+    return(features)
+
+def listing(resampled_audio):
+    x = []
+    for i in range(0, len(resampled_audio), 1000):
+        arr = np.array(resampled_audio[i:i+1000])
+        if len(arr) == 1000:
+            x.append(arr)
+        else:
+            pass
+    x= np.asarray(x)
+    x = x[:-93, :]
+    x = torch.from_numpy(x)
+    return x
+
+def count_labels(labels):
+    count_linking = 0
+    count_flinching = 0
+    for i in range(len(labels)):
+        if labels[i,1] == 0:
+            count_linking = count_linking + 1
+        else:
+            count_flinching = count_flinching + 1
+    print(count_linking)
+    print(count_flinching)
+
+
+def mfcc_lfcc(sampled_array, sample_rate, n_fft=127, hop_length=16, n_lfcc = 64, win_length = None):
+    spectrogram = T.Spectrogram(n_fft=n_fft, hop_length=hop_length)
+    lfcc_spectrogram = T.LFCC(
+        sample_rate=sample_rate,
+        n_lfcc=n_lfcc,
+        speckwargs={
+            "n_fft": n_fft,
+            "win_length": win_length,
+            "hop_length": hop_length,
+        },
+    )
+    
+    audio_feat = []
+    # for i in tqdm(range(len(sampled_array)), desc="Processing"):
+    for i in tqdm(range(len(sampled_array))):
+        mfcc_spec = spectrogram(sampled_array[i])
+        lfcc_spec = lfcc_spectrogram(sampled_array[i])
+        
+        #As the spec will be a dimension of (64, 63) So we have done a pad of 1 row with zero
+        padding = [0, 1]
+        mfcc_spec = Ff.pad(mfcc_spec, padding, mode='constant', value=0)
+        mfcc_spec = mfcc_spec.numpy()
+        lfcc_spec = Ff.pad(lfcc_spec, padding, mode='constant', value=0)
+        lfcc_spec = lfcc_spec.numpy()
+        audio_feat.append([mfcc_spec, lfcc_spec])
+    return audio_feat
+
+def feature_extraction(audio_path):
+    waveform, sample_rate = audio_trimming(audio_path, 653)
+    resampled_audio = F.resample(waveform, sample_rate, 30000, rolloff=0.99)
+    sampled_array = listing(resampled_audio)
+    features = mfcc_lfcc(sampled_array, 30000)
+    features = np.asarray(features)
+    # features = torch.from_numpy(features)
+    return(features)
 
 class MouseDataset(Dataset):
     def __init__(self, frames_folder, pred_path, label_path, audio_path, resampling_rate):
         super(MouseDataset, self).__init__()
 
         self.image_files = [f for f in os.listdir(frames_folder) if os.path.isfile(os.path.join(frames_folder, f))]
+        self.image_files = natsorted(self.image_files)
+        self.image_files = self.image_files[19465:]
         self.frames_folder = frames_folder
         print(self.frames_folder)
         self.pred_path = pred_path
@@ -98,48 +255,27 @@ class MouseDataset(Dataset):
         self.audio_path = audio_path
 
         self.sliding_window = True
-        self.step = 10
+        self.step = 30
         self.stride = 10
         self.bias = len(self.image_files) % self.step
-        self.resampling_rate = resampling_rate
+        # self.resampling_rate = resampling_rate
         self.transform = transforms.Compose([
             transforms.Resize((256, 256)),
             transforms.ToTensor()
         ])
 
-        # load audio
-        self.waveform, self.sample_rate = torchaudio.load(self.audio_path)
-        resampled_audio = F.resample(self.waveform, self.sample_rate, self.resampling_rate, rolloff=0.99)
+        # audio feature extraction in the form of (64, 64) for mfcc and lfcc
         
-        audio_tensor = []
-        for i in range (4650, len(resampled_audio[0]), 50):
-            arr = np.array(resampled_audio[0, i:i+50])
-            if(len(arr)==50):
-                audio_tensor.append(arr)
-            else:
-                pass
-        audio_array = np.asarray(audio_tensor)
-        #audio_array = np.asarray(audio_tensor)
-        # load pose prediction
+        audio_features  = feature_extraction(self.audio_path)
         pose_pred = self.load_predictions(len(self.image_files))
         pose_keypoints = interploate_pose(pose_pred)
 
-        # load labels
-        #check the rame folder path
-        '''
-        if 'CQ' in self.frames_folder:
-            print('CQ')
-            label_data = self.load_CQ_labels(len(self.image_files))
-        elif 'Formalin' in self.frames_folder:
-            print('Formalin')
-            label_data = self.load_Formalin_labels(len(self.image_files))
-        else:
-            print("Your frame folder doesn;'t have either CQ nor Formaline")
-        '''
         label_data = self.load_Formalin_labels(len(self.image_files))
+        
         # sliding window
         if self.sliding_window:
-            self.behavior_feat, self.audio_feat, self.labels, self.valid_indices = sliding_window(pose_keypoints, audio_array, label_data)
+            self.behavior_feat, self.audio_feat, self.labels, self.valid_indices = sliding_window(pose_keypoints, audio_features, label_data)
+
         self.all_indices = np.arange((len(self.image_files) - self.bias - self.step) // self.stride + 1)
 
     def read_label(self, image_file):
@@ -147,6 +283,7 @@ class MouseDataset(Dataset):
         return int(label)
 
     def load_predictions(self, total_frames):
+        #print(total_frames)
         with open(self.pred_path) as f:
             pose_top = json.load(f)
             # print(len(pose_top))
@@ -156,38 +293,18 @@ class MouseDataset(Dataset):
         # Sort annotations
         for i in range(len(pose_top)):
             image_id = pose_top[i]['image_id']
-            if not pose_pred[image_id] == [0]:
-                if pose_top[i]['score'] > pose_pred[image_id]['score']:
+            #print(pose_pred[image_id])
+            #print([image_id, total_frames])
+            if image_id < total_frames:
+                if not pose_pred[image_id] == [0]:
+                    if pose_top[i]['score'] > pose_pred[image_id]['score']:
+                        pose_pred[image_id] = pose_top[i]
+                else:
                     pose_pred[image_id] = pose_top[i]
             else:
-                pose_pred[image_id] = pose_top[i]
-
-        # multi-mouse pose_pred
-        # pose_pred = [[0]]*np.array(video_data).shape[0]
-        # # sort annotations
-        # for i in range(len(pose_top)):
-        #     image_id = pose_top[i]['image_id']
-        #     if pose_pred[image_id] == [0]:
-        #         pose_pred[image_id] = [pose_top[i]]
-        #     else:
-        #         pose_pred[image_id].append(pose_top[i])
+                continue
 
         return pose_pred
-
-    def load_CQ_labels(self, total_frames):
-        label_dataframe = pd.read_excel(self.label_path)
-        
-        label_data = ['no behavior'] * total_frames
-        for index, record in label_dataframe.iterrows():
-            start_frame = time_to_frame(record[0])
-            if pd.isna(record[1]) and index + 1 < len(label_dataframe):
-                end_frame = time_to_frame(label_dataframe.iloc[index + 1, 0])
-            else:
-                end_frame = time_to_frame(record[1])
-            behavior = record[2]
-            for i in range(start_frame, end_frame):
-                label_data[i] = behavior
-        return label_data
 
     def load_Formalin_labels(self, total_frames):
         label_dataframe = pd.read_csv(self.label_path)
@@ -205,7 +322,8 @@ class MouseDataset(Dataset):
         return np.sum(self.valid_indices)
 
     def __getitem__(self, idx):
-        # load image
+
+    
         actual_idx = np.where(self.all_indices)[0][idx]
         start_idx = self.bias + actual_idx * self.stride
         end_idx = start_idx + self.step
@@ -235,4 +353,4 @@ class MouseDataset(Dataset):
         audio_feat = self.audio_feat[idx]   
         audio_feat_tensor = torch.tensor(audio_feat)
 
-        return images_tensor, behavior_feat_tensor, audio_feat, labels_tensor
+        return images_tensor, behavior_feat_tensor, audio_feat_tensor, labels_tensor
--- model.py
+++ model.py
@@ -1,10 +1,29 @@
 import torch
 import torch.nn as nn
 import torchvision.models as models
+import torch.nn.functional as F
 
 class MouseModel(nn.Module):
-    def __init__(self, num_meta_features, num_classes):
+    def __init__(self, num_meta_features, num_audio_features, num_classes):
         super(MouseModel, self).__init__()
+
+        self.vision_model = VisionModel(num_meta_features, num_classes)
+        self.audio_model = AudioModel(num_audio_features, num_classes)
+        
+        self.classifier = nn.Linear(256*2 + 512, num_classes)
+
+    def forward(self, image, meta, audio):
+        vision_output = self.vision_model.forward_features(image, meta)
+        audio_output = self.audio_model.forward_features(audio)
+
+        combined = torch.cat((vision_output, audio_output), dim=1)
+        output = self.classifier(combined)
+        
+        return output
+    
+class VisionModel(nn.Module):
+    def __init__(self, num_meta_features, num_classes):
+        super(VisionModel, self).__init__()
 
         #self.resnet = models.resnet18(pretrained=True)
         #self.resnet = nn.Sequential(*list(self.resnet.children())[:-1])
@@ -29,9 +48,9 @@ class MouseModel(nn.Module):
 
         self.classifier = nn.Linear(256*2, num_classes)
 
-    def forward(self, audio):
-        #batch_size, num_frames, c, h, w = image.shape
-        #image = image.view(batch_size*num_frames, c, h, w)
+    def forward_features(self, image, meta):
+        batch_size, num_frames, c, h, w = image.shape
+        image = image.view(batch_size*num_frames, c, h, w)
 
         #image = self.resnet(image)
         #image = image.view(batch_size, num_frames, -1)
@@ -42,7 +61,83 @@ class MouseModel(nn.Module):
         meta = self.audio_fc(audio)
         meta = meta.mean(dim=1)
 
-        #combined = torch.cat((image, meta), dim=1)
-        
-        output = self.classifier(meta)
+        combined = torch.cat((image, meta), dim=1)
+        return combined
+
+    def forward(self, image, meta):
+        combined = self.forward_features(image, meta)
+        output = self.classifier(combined)
         return output
+    
+
+class AudioModel(nn.Module):
+    def __init__(self, num_audio_features, num_classes):
+        super(AudioModel, self).__init__()
+
+        self.conv1 = nn.Conv3d(2, 8, kernel_size=(3,3, 3), stride=1, padding=1)
+        self.act1 = nn.ReLU()
+        self.bn1 = nn.BatchNorm3d(8)
+        self.drop1 = nn.Dropout(0.5)
+ 
+        self.conv2 = nn.Conv3d(8, 16, kernel_size=(3, 3, 3), stride=1, padding=1)
+        self.act2 = nn.ReLU()
+        # self.bn2 = nn.BatchNorm3d(16)
+        self.pool2 = nn.MaxPool3d(kernel_size=(2, 2, 2))
+
+        self.conv3 = nn.Conv3d(16, 32, kernel_size=(3,3,3), stride=1, padding=1)
+        self.act3 = nn.ReLU()
+        # self.bn3 = nn.BatchNorm3d(32)
+        self.pool3 = nn.MaxPool3d(kernel_size=(2, 2, 2))
+        # self.drop3 = nn.Dropout(0.5)
+
+        self.conv4 = nn.Conv3d(32, 64, kernel_size=(3,3,3), stride=1, padding=1)
+        self.act4 = nn.ReLU()
+        # self.bn4 = nn.BatchNorm3d(64)
+        self.pool4 = nn.MaxPool3d(kernel_size=(2, 2, 2))
+        # self.pool4 = nn.MaxPool3d(kernel_size=(2, 2, 2))
+
+        self.conv5 = nn.Conv3d(64, 128, kernel_size=(3,3,3), stride=1, padding=1)
+        self.act5 = nn.ReLU()
+        self.pool5 = nn.MaxPool3d(kernel_size=(1, 1, 1))
+
+        self.flat = nn.Flatten()
+        self.drop7 = nn.Dropout(0.5)
+
+        self.fc3 = nn.Linear(24576, 512)
+        self.act3 = nn.ReLU()
+
+        self.drop6 = nn.Dropout(0.5)
+ 
+        self.fc6 = nn.Linear(512, 2)
+ 
+    def forward(self, x):
+        # input stepx2x64x64, output 32x32x32
+        batch_size, num_frames, c, h, w = x.shape
+        x = x.view(batch_size, c, num_frames, h, w)
+
+        x = self.act1(self.conv1(x))
+        x = self.bn1(x)
+        x = self.drop1(x)
+
+        x = self.act2(self.conv2(x))
+        x = self.pool2(x)
+
+        x = self.act3(self.conv3(x))
+        x = self.pool3(x)
+
+        x = self.act4(self.conv4(x))
+        x = self.pool4(x)
+
+        x = self.act5(self.conv5(x))
+        x = self.pool5(x)
+
+        
+        x = self.flat(x)
+        x = self.drop7(x)
+        x = self.act3(self.fc3(x))
+
+        x = self.drop6(x)
+        
+        x = self.fc6(x)
+        return x
+
